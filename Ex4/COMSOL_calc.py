1ac14d6732fda2d3bad31b39d18eae94e2201c59
shrimpachi <131880385+shrimpachi@users.noreply.github.com>
2024-10-28 18:10:14 +0100

    Added the comsol c1 and c2 in txt and excel, and added the script to run it

---
+++ Ex4/COMSOL_calc.py
@@ -0,0 +1,27 @@
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+
+file = "COMSOL_plot.xlsx"
+df = pd.read_excel(file)
+
+r = df["R"]
+c1 = df["c1"]
+c2 = df["c2"]
+L = 0.01/2
+kA = 2
+kB = 0.5
+Deff = 1.18e-6
+
+
+rateB = 0
+rateC = 0
+for i in range(len(r)):
+	rateB += (kA*c1[i]) - (kB*c2[i])
+	rateC += kB*c2[i]
+
+selectivity = rateC/rateB
+
+
+print(f"The selectivity value is {selectivity}")
+
